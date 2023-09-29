c57a0ac37b5b1c9969d41e9829772281d16b4467
jchancellor-ms <jchancellor@microsoft.com>
2023-09-29 09:44:50 -0700

    corrected telemetry bugs

--- locals.telemetry.tf
+++ locals.telemetry.tf
@@ -10,7 +10,7 @@ locals {
   module_type = "res"
 
   # This ensures we don't get errors if telemetry is disabled.
-  telem_random_hex = can(random_id.telem[0].hex) ? random_id.telem[0].hex : ""
+  telem_random_hex = can(random_id.telemetry[0].hex) ? random_id.telemetry[0].hex : ""
 
   # This constructs the ARM deployment name that is used for the telemetry.
   # We shouldn't ever hit the 64 character limit but use substr just in case.
