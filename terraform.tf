67f3be65486309b057a8a761e942cb5bde56a505
jchancellor-ms <jchancellor@microsoft.com>
2024-02-08 16:16:57 -0800

    version update, and tf lint fixes

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = "~> 1.6"
   required_providers {
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~> 1.12"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.90"
