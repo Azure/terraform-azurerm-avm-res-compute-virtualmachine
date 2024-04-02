43c65082cf0333acf430561766da26648c7905a9
lolorol <LaurentLesle@users.noreply.github.com>
2024-04-03 03:16:31 +0800

    Add examples - winrm https and openssh for windows (#47)

--- examples/basic_windows_w_load_balancing/main.tf
+++ examples/basic_windows_w_load_balancing/main.tf
@@ -138,68 +138,61 @@ module "loadbalancer" {
 # copied over from the AzureRM example - simplifies naming for the appgw resources
 locals {
   backend_address_pool_name      = "${azurerm_virtual_network.this_vnet.name}-beap"
-  frontend_port_name             = "${azurerm_virtual_network.this_vnet.name}-feport"
   frontend_ip_configuration_name = "${azurerm_virtual_network.this_vnet.name}-feip"
+  frontend_port_name             = "${azurerm_virtual_network.this_vnet.name}-feport"
   http_setting_name              = "${azurerm_virtual_network.this_vnet.name}-be-htst"
   listener_name                  = "${azurerm_virtual_network.this_vnet.name}-httplstn"
   request_routing_rule_name      = "${azurerm_virtual_network.this_vnet.name}-rqrt"
 }
 
 resource "azurerm_application_gateway" "network" {
+  location            = azurerm_resource_group.this_rg.location
   name                = "example-appgateway"
   resource_group_name = azurerm_resource_group.this_rg.name
-  location            = azurerm_resource_group.this_rg.location
-
-  sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 2
-  }
-
-  gateway_ip_configuration {
-    name      = "my-gateway-ip-configuration"
-    subnet_id = azurerm_subnet.this_subnet_2.id
-  }
-
-  frontend_port {
-    name = local.frontend_port_name
-    port = 80
-  }
-
-  frontend_ip_configuration {
-    name                          = local.frontend_ip_configuration_name
-    subnet_id                     = azurerm_subnet.this_subnet_2.id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.0.3.100"
-  }
 
   backend_address_pool {
     name = local.backend_address_pool_name
   }
-
   backend_http_settings {
-    name                  = local.http_setting_name
     cookie_based_affinity = "Disabled"
-    path                  = "/path1/"
+    name                  = local.http_setting_name
     port                  = 80
     protocol              = "Http"
+    path                  = "/path1/"
     request_timeout       = 60
   }
-
+  frontend_ip_configuration {
+    name                          = local.frontend_ip_configuration_name
+    private_ip_address            = "10.0.3.100"
+    private_ip_address_allocation = "Static"
+    subnet_id                     = azurerm_subnet.this_subnet_2.id
+  }
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.this_subnet_2.id
+  }
   http_listener {
-    name                           = local.listener_name
     frontend_ip_configuration_name = local.frontend_ip_configuration_name
     frontend_port_name             = local.frontend_port_name
+    name                           = local.listener_name
     protocol                       = "Http"
   }
-
   request_routing_rule {
-    name                       = local.request_routing_rule_name
-    priority                   = 9
-    rule_type                  = "Basic"
     http_listener_name         = local.listener_name
+    name                       = local.request_routing_rule_name
+    rule_type                  = "Basic"
     backend_address_pool_name  = local.backend_address_pool_name
     backend_http_settings_name = local.http_setting_name
+    priority                   = 9
+  }
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
   }
 }
 
@@ -266,8 +259,8 @@ module "testnsg" {
 }
 
 resource "azurerm_application_security_group" "test_asg" {
-  name                = module.naming.application_security_group.name_unique
   location            = azurerm_resource_group.this_rg.location
+  name                = module.naming.application_security_group.name_unique
   resource_group_name = azurerm_resource_group.this_rg.name
 }
 
