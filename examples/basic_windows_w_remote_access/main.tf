afbbd41b34567e450885e05af1d13c6381ec47df
Jon Chancellor <jchancellor@microsoft.com>
2025-04-10 12:22:31 -0700

    add key vault purge

--- examples/basic_windows_w_remote_access/main.tf
+++ examples/basic_windows_w_remote_access/main.tf
@@ -18,6 +18,9 @@ provider "azurerm" {
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+    }
   }
 }
 
