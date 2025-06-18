66e3ee0c4cef0b68e9a58f33a2592622b663d042
Jon Chancellor <jchancellor@microsoft.com>
2025-06-18 13:18:24 -0700

    maint config to azapi

--- main.maintenance_configurations.tf
+++ main.maintenance_configurations.tf
@@ -1,3 +1,4 @@
+/*
 resource "azurerm_maintenance_assignment_virtual_machine" "this" {
   for_each = var.maintenance_configuration_resource_ids
 
@@ -9,4 +10,25 @@ resource "azurerm_maintenance_assignment_virtual_machine" "this" {
     azurerm_virtual_machine_data_disk_attachment.this_linux,
     azurerm_virtual_machine_data_disk_attachment.this_windows
   ]
+}
+*/
+
+resource "azapi_resource" "this_maintenance_configuration_assignment" {
+  type     = "Microsoft.Maintenance/configurationAssignments@2023-04-01"
+  for_each = var.maintenance_configuration_resource_ids
+
+  parent_id = local.virtualmachine_resource_id
+  name      = "${var.name}-maintenance-configuration-${each.key}"
+
+  location = var.location
+  body = {
+    properties = {
+      maintenanceConfigurationId = each.value
+    }
+  }
+}
+
+moved {
+  from = azurerm_maintenance_assignment_virtual_machine.this
+  to   = azapi_resource.this_maintenance_configuration_assignment
 }
